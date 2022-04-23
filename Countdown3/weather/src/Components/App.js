a4ed6e0954d70197a4c38a38517175e7f77f9aea
AFlint0n <AFlint0n@users.noreply.github.com>
2022-04-23 13:48:51 -0400

    init commit

---
+++ Countdown3/weather/src/Components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+import {useEffect, useState} from 'react';
+import Weather from './Weather.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Weather
+      </header>
+      <>
+        <Weather></Weather>
+      </>
+    </div>
+  );
+}
+
+export default App;
---
+++ Countdown3/weather/src/Components/Weather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState, useEffect } from "react";
+import '../App.css';
+
+function Weather (props) {
+    return(
+    <>
+       <p>Test</p>
+    </>
+    );
+}
+
+export default Weather;
